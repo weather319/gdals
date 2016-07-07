b6bb281b6ea90e16903197a1667e16b325233ea9
Weather319 <xjzcm717@163.com>
2016-07-07 18:13:57 +0800

    load_data

--- load_training_data.py
+++ load_training_data.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from screen_excel import data_excel
 import os
-
+import tarfile
 
 
 def read_file(path):
@@ -12,17 +13,25 @@ def read_file(path):
 def data_clean(filelists,):
     pass
 
+def untar(file_name_list, dirs):
+    for file_name in file_name_list:
+        t = tarfile.open(file_name)
+        t.extractall(path=dirs)
+
+def load_data():
+    pass
+
+
 
 
 if __name__ == '__main__':
-    path = 'C:\zhut11\data'
-    excel_path = 'C:\zhut11\python\gdals\TH.xlsx'
+    path = '../data'
+    excel_path = 'TH.xlsx'
     print (os.path.abspath(path))
     filelists, mapid_lists = read_file(path)
     WaterQualiteId_lists = ['叶绿素a','CODM']
     WQ = data_excel(excel_path=excel_path)
     WQ.screen_Excel(mapid_lists,WaterQualiteId_lists)
     print (WQ.water_df.head())
-    WQ.save_excel(path="C:\zhut11\python")
-    
-    #print (filelists,mapid_lists)
+    #WQ.save_excel(path=".")
+    print (filelists,mapid_lists)
